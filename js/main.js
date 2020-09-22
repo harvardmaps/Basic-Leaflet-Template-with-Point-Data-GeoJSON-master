cbb6ac4425e8f23cc09e449e3c3a0095bf401ea1
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-09-22 12:02:45 -0400

    historical layers

--- js/main.js
+++ js/main.js
@@ -25,6 +25,34 @@ function getData(map){
 		ext: 'png'
 }).addTo(historicalDataMap);
 
+var controlLayers = L.control.layers( null, null, {
+    position: "topright",
+    collapsed: false
+  }).addTo(mymap);
+
+var Beers_1874 = L.tileLayer(
+	'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059015410/tiles/{z}/{x}/{-y}.png', {
+		tms: true, 
+		attribution: 'Leventhal Map & Education Center'
+	}
+);
+controlLayers.addBaseLayer(Beers_1874, '1874 F.W. Beers & Co');
+
+var Sanborn_1867 = L.tileLayer(
+	'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059012052/tiles/{z}/{x}/{-y}.png', {
+		tms: true, 
+		attribution: 'Leventhal Map & Education Center'
+	}
+).addTo(mymap);
+
+controlLayers.addBaseLayer(Sanborn_1867, '1867 Sanborn');
+
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+ });
+ 
+ controlLayers.addBaseLayer(OpenTopoMap, 'Open Topo Map');
 
 
 //part that gets the school data
