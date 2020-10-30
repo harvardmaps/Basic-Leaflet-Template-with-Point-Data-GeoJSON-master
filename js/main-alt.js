2f5bf64dbca9a80a678ee27756cfa912be228b48
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-10-30 15:47:51 -0400

    add keypress

--- js/main-alt.js
+++ js/main-alt.js
@@ -1,76 +1,6 @@
 // declare map in global scope
 var historicalDataMap;
 
-
-//create sidebar content
-function whenClicked(e) {
-    // e = event
-    var feature = e.target;
-    // Build the display name
-	if (feature.feature.properties.NAME_2 == null){
-		display_name = feature.feature.properties.NAME_1
-	} else {
-		if (feature.feature.properties.NAME_1 == null){
-			display_name = feature.feature.properties.NAME_2
-		} else {
-			display_name = feature.feature.properties.NAME_2 + " " + feature.feature.properties.NAME_1
-			}
-        }
-    
-    // Build the display date
-	if (feature.feature.properties.START_LOC == null && feature.feature.properties.END_LOC == null){
-		date = "no date available."
-	} else {
-		if (feature.feature.properties.START_LOC == null){
-			date = "in " + feature.feature.properties.END_LOC + "." 
-		} else {
-			if (feature.feature.properties.END_LOC == null){
-				date = "since " + feature.feature.properties.START_LOC + "." 
-			} else {
-				date = "from " + feature.feature.properties.START_LOC + " to " + feature.feature.properties.END_LOC + "."
-			}
-		}
-    }
-    
-    // Build the display notes and source
-    if (feature.feature.properties.NOTES == null){
-        notes = ""
-    } else{
-        notes = "<br><strong>Notes:</strong> " + feature.feature.properties.NOTES
-    }
-    if (feature.feature.properties.SOURCE == null){
-        source = ""
-    } else {
-        source = "<br><strong>Source:</strong> " + feature.feature.properties.SOURCE
-    }
-
-    // Build the display record links
-	if (feature.feature.properties.TITLE_1 == ""){
-		r_link_1 = ""
-	} else {
-		r_link_1 = "<p>Records in HOLLIS include: <p>" + "<li><a href='" + feature.feature.properties.URL_1 + "'>" 
-		+ feature.feature.properties.TITLE_1 + "</a></li>"
-	}
-	if (feature.feature.properties.TITLE_2 == ""){
-		r_link_2 = ""
-	} else {
-		r_link_2 = "<li><a href='" + feature.feature.properties.URL_2 + "'>" 
-		+ feature.feature.properties.TITLE_2 + "</a></li>"
-	}
-	if (feature.feature.properties.TITLE_3 == ""){
-		r_link_3 = ""
-	} else {
-		r_link_3 = "<li><a href='" + feature.feature.properties.URL_3 + "'>" 
-		+ feature.feature.properties.TITLE_3 + "</a></li>"
-	}
-    
-    action = feature.feature.properties.ACTION
-    type = feature.feature.properties.Type;
-
-    content = display_name + " " + action + " here " + date + r_link_1 + r_link_2 + r_link_3 + notes + source;
-    $( "#sidebar-content" ).html("<h2>Information on this location</h2>" + content);
-    };
-
 //function to add the data to the map with the display content
 function dataLayer(data, map) {
 	
@@ -111,8 +41,7 @@ function dataLayer(data, map) {
 	var layerAll = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -123,8 +52,7 @@ function dataLayer(data, map) {
     var layer1830 = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -138,8 +66,7 @@ function dataLayer(data, map) {
 	var layer1840 = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -153,8 +80,7 @@ function dataLayer(data, map) {
 	var layer1850 = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -168,8 +94,7 @@ function dataLayer(data, map) {
 	var layer1860 = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -183,8 +108,7 @@ function dataLayer(data, map) {
 	var layer1870 = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -198,8 +122,7 @@ function dataLayer(data, map) {
 	var layerResources = L.geoJson(data, {
         onEachFeature: function(feature, layer) {
             layer.on({
-				click: whenClicked,
-				keypress: whenClicked
+                click: whenClicked
             })
 		},
 		pointToLayer : function (feature, latlng) {
@@ -242,66 +165,28 @@ function dataLayer(data, map) {
 		"Has Library Resource": markersResources
 	};
 
-	L.control.layers(dataLayers, overlays, { collapsed: false }).addTo(map);
+    L.control.layers(dataLayers, overlays, { collapsed: false }).addTo(map);
 }
 
-function removeFeatures() {
-	$( "#sidebar-content" ).html("<h2>Introduction to the site</h2> <p>Some ideas here</p>");
-};
-
-$( "#reset-button" ).click(function() {
-	removeFeatures();
+var datapoints = $.ajax({
+    url: "data/historical_data.geojson",
+    dataType: "json",
+    success: console.log("tracts successfully loaded."),
+    error: function(xhr) {
+        alert(xhr.statusText)
+    }
 });
 
-//instantiate map 
-function createMap(){
-	 historicalDataMap = L.map('map',{
+$.when(datapoints).done(function() {
+    historicalDataMap = L.map('map',{
 		center: [42.350,-71.065],
 		zoom: 14,
 		minZoom: 4,
 		maxZoom: 18,
 		zoomControl:false
-	});
+    });
 
-	//call getdata function
-	getData(historicalDataMap);
-	historicalDataMap.addControl( L.control.zoom({position: 'bottomright'}) )
+    historicalDataMap.addControl( L.control.zoom({position: 'bottomright'}) );
+
+    dataLayer(datapoints, map)
 };
-
-//function to retrieve map data and place it on the map
-function getData(map){
-	//baselayer
-    var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        subdomains: 'abcd',
-        minZoom: 0,
-        maxZoom: 21,
-        ext: 'png'
-    }).addTo(map);
-
-
-
-//part that gets the data
- $.ajax("data/historical_data.geojson",{
-	dataType: "json",
-	 success: function(response){
-		 var datapoints = response.features;
-
-		//function for sidebar content
-		dataLayer(datapoints, map)
-
-
-
-// THREE IMPORTANT CLOSING BRACKETS AT THE END OF GETDATA() FUNCTION!
-//bracket that closes out the async response. Don't erase!
-}
-
-//bracket that closes out the async call function. Don't erase!
-});
-
-
-//bracket that closes out the getData() function. Don't erase!
-};
-
-
-$(document).ready(createMap);
