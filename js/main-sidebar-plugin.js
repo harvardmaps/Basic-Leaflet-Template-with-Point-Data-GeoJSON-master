32bc32d103c6d601aed943f1688fbf29b8ea2baa
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-11-17 13:36:25 -0500

    revert

--- js/main-sidebar-plugin.js
+++ js/main-sidebar-plugin.js
@@ -72,210 +72,7 @@ function whenClicked(e) {
 	'<h2>Information about this place</h2><br><strong>Name: ' + content);
 };
 
-//function to add the data to the map with the display content
-function dataLayer(data, map) {
-	
-	// attempt to define overlays and overlay group within the function
-	var Sanborn_1867 = L.tileLayer('https://s3.us-east-2.wasabisys.com/urbanatlases/39999059012052/tiles/{z}/{x}/{y}.png', {
-		attribution: 'Leventhal Map & Education Center'
-	});
 
-	var Beers_1874 = L.tileLayer(
-		'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059015410/tiles/{z}/{x}/{-y}.png', {
-			tms: true, 
-			attribution: 'Leventhal Map & Education Center'
-		}
-	);
-
-    var Hopkins_1874 = L.tileLayer(
-		'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059010650/tiles/{z}/{x}/{y}.png', {
-			attribution: 'Leventhal Map & Education Center'
-		}
-	);
-	
-	var McIntyre_1852 = L.tileLayer(
-		'https://bburns.share.library.harvard.edu/tmsprojects/mcintyre_tiles/{z}/{x}/{y}.png', {	
-		//'https://bburns.share.library.harvard.edu/tmsprojects/McIntyreXYZ/{z}/{x}/{y}.png', {
-			tms: true,
-			attribution: 'Harvard Map Collection'
-		}
-    );
-	
-	var noLayer = L.tileLayer('');
-
-	var overlays = {
-		"McIntyre, 1852" : McIntyre_1852,
-    	"Hopkins, 1874" : Hopkins_1874,
-		"F. W. Beers & Co., 1874" : Beers_1874,
-		"Sanborn, 1867" : Sanborn_1867,
-		"None" : noLayer
-	};
-	
-	var geojsonIcon = L.divIcon( {className: 'geojson-Icon'});
-	
-	//Call all the geoJson data as individual layers for the map. 
-	//Add filter options and event functions. Use marker not circlemarker to get keyboard access.
-	var layerAll = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		}
-	});
-
-    var layer1830 = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		},
-		filter: function(feature) {
-			return feature.properties.END_LOC <= 1830
-		}
-	});
-
-	var layer1840 = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		},
-		filter: function(feature) {
-			return feature.properties.END_LOC <= 1840 && feature.properties.END_LOC > 1830
-		}
-	});
-
-	var layer1850 = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		},
-		filter: function(feature) {
-			return feature.properties.END_LOC <= 1850 && feature.properties.END_LOC > 1840
-		}
-	});
-
-	var layer1860 = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		},
-		filter: function(feature) {
-			return feature.properties.END_LOC <= 1860 && feature.properties.END_LOC > 1850
-		}
-	});
-
-	var layer1870 = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		},
-		filter: function(feature) {
-			return feature.properties.END_LOC <= 1870 && feature.properties.END_LOC > 1860
-		}
-	});
-
-	var layerResources = L.geoJson(data, {
-        onEachFeature: function(feature, layer) {
-            layer.on({
-				click: whenClicked,
-				keypress: whenClicked
-            })
-		},
-		pointToLayer : function (feature, latlng) {
-			return L.marker(latlng, {icon: geojsonIcon});
-		},
-		filter: function(feature) {
-			return feature.properties.TITLE_1 != ""
-		}
-	});
-	
-	//Create markercluster groups for all the layers. Add a keypress listener for each cluster
-	var markersAll = L.markerClusterGroup();
-	markersAll.addLayer(layerAll);
-	markersAll.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-
-	map.addLayer(markersAll);
-	
-	var markers1830 = L.markerClusterGroup();
-	markers1830.addLayer(layer1830);
-	markers1830.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-
-	var markers1840 = L.markerClusterGroup();
-	markers1840.addLayer(layer1840);
-	markers1840.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-
-	var markers1850 = L.markerClusterGroup();
-	markers1850.addLayer(layer1850);
-	markers1850.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-
-	var markers1860 = L.markerClusterGroup();
-	markers1860.addLayer(layer1860);
-	markers1860.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-
-	var markers1870 = L.markerClusterGroup();
-	markers1870.addLayer(layer1870);
-	markers1870.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-
-	var markersResources = L.markerClusterGroup();
-	markersResources.addLayer(layerResources);
-	markersResources.on('clusterkeypress', function (a) {
-		a.layer.zoomToBounds();
-	});
-	
-	//Add layers to controls and add controls to the map
-	var dataLayers = {
-		"All data": markersAll,
-		"Pre-1830": markers1830,
-		"1831-1840": markers1840,
-		"1841-1850": markers1850,
-		"1851-1860": markers1860,
-		"1861-1870": markers1870,
-		"Has Library Resource": markersResources
-	};
-
-	var dataControl = L.control.layers(dataLayers, null, { position: 'topright', collapsed: false }).addTo(map);
-	var overlayControl = L.control.layers(overlays, null, { position: 'topright', collapsed: false }).addTo(map);
-}
 
 //instantiate map 
 function createMap(){
@@ -325,8 +122,211 @@ function getData(map){
 	 success: function(response){
 		 var datapoints = response.features;
 
-		//function for sidebar content
-		dataLayer(datapoints, map)
+
+		//function to add the data to the map with the display content
+		function (data, map) {
+			
+			// attempt to define overlays and overlay group within the function
+			var Sanborn_1867 = L.tileLayer('https://s3.us-east-2.wasabisys.com/urbanatlases/39999059012052/tiles/{z}/{x}/{y}.png', {
+				attribution: 'Leventhal Map & Education Center'
+			});
+
+			var Beers_1874 = L.tileLayer(
+				'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059015410/tiles/{z}/{x}/{-y}.png', {
+					tms: true, 
+					attribution: 'Leventhal Map & Education Center'
+				}
+			);
+
+			var Hopkins_1874 = L.tileLayer(
+				'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059010650/tiles/{z}/{x}/{y}.png', {
+					attribution: 'Leventhal Map & Education Center'
+				}
+			);
+			
+			var McIntyre_1852 = L.tileLayer(
+				'https://bburns.share.library.harvard.edu/tmsprojects/mcintyre_tiles/{z}/{x}/{y}.png', {	
+				//'https://bburns.share.library.harvard.edu/tmsprojects/McIntyreXYZ/{z}/{x}/{y}.png', {
+					tms: true,
+					attribution: 'Harvard Map Collection'
+				}
+			);
+			
+			var noLayer = L.tileLayer('');
+
+			var overlays = {
+				"McIntyre, 1852" : McIntyre_1852,
+				"Hopkins, 1874" : Hopkins_1874,
+				"F. W. Beers & Co., 1874" : Beers_1874,
+				"Sanborn, 1867" : Sanborn_1867,
+				"None" : noLayer
+			};
+			
+			var geojsonIcon = L.divIcon( {className: 'geojson-Icon'});
+			
+			//Call all the geoJson data as individual layers for the map. 
+			//Add filter options and event functions. Use marker not circlemarker to get keyboard access.
+			var layerAll = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				}
+			});
+
+			var layer1830 = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				},
+				filter: function(feature) {
+					return feature.properties.END_LOC <= 1830
+				}
+			});
+
+			var layer1840 = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				},
+				filter: function(feature) {
+					return feature.properties.END_LOC <= 1840 && feature.properties.END_LOC > 1830
+				}
+			});
+
+			var layer1850 = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				},
+				filter: function(feature) {
+					return feature.properties.END_LOC <= 1850 && feature.properties.END_LOC > 1840
+				}
+			});
+
+			var layer1860 = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				},
+				filter: function(feature) {
+					return feature.properties.END_LOC <= 1860 && feature.properties.END_LOC > 1850
+				}
+			});
+
+			var layer1870 = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				},
+				filter: function(feature) {
+					return feature.properties.END_LOC <= 1870 && feature.properties.END_LOC > 1860
+				}
+			});
+
+			var layerResources = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.on({
+						click: whenClicked,
+						keypress: whenClicked
+					})
+				},
+				pointToLayer : function (feature, latlng) {
+					return L.marker(latlng, {icon: geojsonIcon});
+				},
+				filter: function(feature) {
+					return feature.properties.TITLE_1 != ""
+				}
+			});
+			
+			//Create markercluster groups for all the layers. Add a keypress listener for each cluster
+			var markersAll = L.markerClusterGroup();
+			markersAll.addLayer(layerAll);
+			markersAll.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+
+			map.addLayer(markersAll);
+			
+			var markers1830 = L.markerClusterGroup();
+			markers1830.addLayer(layer1830);
+			markers1830.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+
+			var markers1840 = L.markerClusterGroup();
+			markers1840.addLayer(layer1840);
+			markers1840.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+
+			var markers1850 = L.markerClusterGroup();
+			markers1850.addLayer(layer1850);
+			markers1850.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+
+			var markers1860 = L.markerClusterGroup();
+			markers1860.addLayer(layer1860);
+			markers1860.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+
+			var markers1870 = L.markerClusterGroup();
+			markers1870.addLayer(layer1870);
+			markers1870.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+
+			var markersResources = L.markerClusterGroup();
+			markersResources.addLayer(layerResources);
+			markersResources.on('clusterkeypress', function (a) {
+				a.layer.zoomToBounds();
+			});
+			
+			//Add layers to controls and add controls to the map
+			var dataLayers = {
+				"All data": markersAll,
+				"Pre-1830": markers1830,
+				"1831-1840": markers1840,
+				"1841-1850": markers1850,
+				"1851-1860": markers1860,
+				"1861-1870": markers1870,
+				"Has Library Resource": markersResources
+			};
+
+			var dataControl = L.control.layers(dataLayers, null, { position: 'topright', collapsed: false }).addTo(map);
+			var overlayControl = L.control.layers(overlays, null, { position: 'topright', collapsed: false }).addTo(map);
+		}
 
 
 
